e6e43aab55abf1994137b74bb4aa122fa76a328b
Haroon Sheikh <Haroon.sheikh0706@gmail.com>
2021-05-10 17:44:41 +0100

    feat: New code written for a basic dice rolling simulator Use of the Random module and loop functions

---
+++ Dicerollsimulator.py
@@ -0,0 +1,18 @@
+# Creating a simple dice rolling generator using the random module 
+
+# instead of importing the entire module we will import the randint method specifically
+
+from random import randint
+
+# Initial question for the simulator
+
+quest = input("Do you want to roll the dice? (Y/N) ")
+
+# Rolling the dice based off the user's input
+
+while quest[0].upper() == "Y":
+    print(f"You rolled a {randint(1, 6)}")
+    quest = input("Would you like to roll again? (Y/N)")
+
+else:
+    print("Thank you for rolling with us!")
